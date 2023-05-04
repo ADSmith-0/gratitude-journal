700167e36ba10ea42c9da294355af249a079e70e
ADSmith-0 <adam00199382818@gmail.com>
2023-05-04 01:56:57 +0100

    added: todo comment to review previous change before launching

--- src/routes/logout/+page.server.js
+++ src/routes/logout/+page.server.js
@@ -1,5 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
+// TODO: potentially change this back to +server.js with GET
 export async function load({ cookies }) {
     const accessToken = cookies.get('accessToken');
 
