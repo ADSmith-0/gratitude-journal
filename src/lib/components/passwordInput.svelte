f3070fd94c0c0ca40f536c9bc4f92c5bb7c9d923
ADSmith-0 <adam00199382818@gmail.com>
2023-05-16 01:03:25 +0100

    changed: passwordInput now has label and name passed through as props, allowing more customisability

--- src/lib/components/passwordInput.svelte
+++ src/lib/components/passwordInput.svelte
@@ -2,11 +2,14 @@
     import IconButton from '@smui/icon-button';
     import CustomInput from './customInput.svelte';
     
+    export let label = "Password";
+    export let name = "password";
+    
     let showPassword = false;
 
     const changeVisibility = () => showPassword = !showPassword;
 </script>
 
-<CustomInput label="Password" name="password" type={showPassword ? "text" : "password"}>
+<CustomInput label={label} name={name} type={showPassword ? "text" : "password"}>
     <IconButton class="material-icons" type="button" on:click={changeVisibility} slot="trailingIcon">{showPassword ? "visibility_off" : "visibility"}</IconButton>
 </CustomInput>
