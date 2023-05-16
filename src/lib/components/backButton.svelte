1df88547458a6e480f8902f01a544b5bfddcc192
ADSmith-0 <adam00199382818@gmail.com>
2023-05-16 01:04:19 +0100

    created: back button component for navigation

---
+++ src/lib/components/backButton.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Icon } from "@smui/icon-button";
+
+    export let href = "";
+</script>
+<a class="wrapper button small bg-white no-shadow" href={href}>
+    <Icon class="material-icons">arrow_back</Icon>
+</a>
+<style>
+    .wrapper {
+        margin-left: 1rem;
+        margin-top: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 1rem;
+    }
+</style>
